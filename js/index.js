26179f2c98d78170ff530119fe060cf802754bc8
Denisouls <denilson.santana@sempreceub.com>
2024-08-29 10:40:52 -0300

    Atualização

--- js/index.js
+++ js/index.js
@@ -2,9 +2,16 @@ const diaSemana = document.getElementById("dia-semana");
 const diaMesAno = document.getElementById("dia-mes-ano");
 const horaMinSeg = document.getElementById("hora-min-seg");
 
+//Todo conjunto numérico(exceto ano) deve ter 2 dígitos (adicionar 0 se for menor que 10)
+//Retornar dia da semana por extenso (em pt-br)
+
+function diaSemana() {
+    //retornar dia da semana
+}
+
 function dataCompleta() {
     const date = new Date();
-    return date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+    return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
 }
 
 function horaCompleta() {
@@ -14,3 +21,4 @@ function horaCompleta() {
 
 diaMesAno.textContent = dataCompleta();
 horaMinSeg.textContent = horaCompleta();
+
