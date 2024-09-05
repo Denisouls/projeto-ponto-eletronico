1d51d27fea7eab0f34d9f5cc900edb28435d04cc
Denisouls <denilson.santana@sempreceub.com>
2024-09-05 10:39:24 -0300

    Aula 05/09

--- js/index.js
+++ js/index.js
@@ -1,12 +1,25 @@
 const diaSemana = document.getElementById("dia-semana");
 const diaMesAno = document.getElementById("dia-mes-ano");
 const horaMinSeg = document.getElementById("hora-min-seg");
+const arrayDayWeek = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
 
+const dialogPonto = document.getElementById("dialog-ponto");
+
+const btnRegistrarPonto = document.getElementById("btn-registrar-ponto");
+btnRegistrarPonto.addEventListener("click", () => {
+    dialogPonto.showModal();
+});
+
+const btnDialogFechar = document.getElementById("btn-dialog-fechar");
+btnDialogFechar.addEventListener("click", () => {
+    dialogPonto.close();
+})
 //Todo conjunto numérico(exceto ano) deve ter 2 dígitos (adicionar 0 se for menor que 10)
 //Retornar dia da semana por extenso (em pt-br)
 
-function diaSemana() {
-    //retornar dia da semana
+function daySemana() {
+    const date = new Date();
+        return arrayDayWeek[date.getDay()];
 }
 
 function dataCompleta() {
@@ -19,6 +32,11 @@ function horaCompleta() {
     return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
 
-diaMesAno.textContent = dataCompleta();
-horaMinSeg.textContent = horaCompleta();
+function atualizaHora() {
+    horaMinSeg.textContent = horaCompleta();
+}
 
+setInterval(atualizaHora, 1000)
+
+diaSemana.textContent = daySemana();
+diaMesAno.textContent = dataCompleta();
